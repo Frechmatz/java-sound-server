cd928827f22bbf464e4db0f022091de48c0604fe
Oliver Voeltz <frechmatz@gmx.de>
2021-07-23 17:50:52 +0200

    Reowrked exception handling

--- src/main/java/de/frechmatz/javasoundserver/Application.java
+++ src/main/java/de/frechmatz/javasoundserver/Application.java
@@ -17,55 +17,39 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Application {
-
     private static final Logger logger = LogManager.getLogger(Application.class);
 
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        int port = args.length > 0 ? Integer.parseInt(args[0]) : 9000;
-        try {
-            logger.info("Starting server on port " + port);
-            serverSocket = new ServerSocket(port);
-            while (true) {
-                logger.info("Accepting connection...");
-                Socket socket = serverSocket.accept();
-                logger.info("Connection established");
+    public static void main(String[] args) throws IOException {
+        final int port = args.length > 0 ? Integer.parseInt(args[0]) : 9000;
+        final ServerSocket serverSocket = new ServerSocket(port);
+        while (true) {
+            logger.info(String.format("Accepting connection on port %s...", port));
+            Socket socket = serverSocket.accept();
+            logger.info("Connection established");
+            try {
+                final Session session = SessionFactory.instantiate(socket.getInputStream());
+                if (!(session.getMajorVersion() == 1 && session.getMinorVersion() == 0))
+                    throw new Exception("Version not supported");
+                logger.info("Accepted " + session);
+                socket.getOutputStream().write(0); // Accepted
+                socket.getOutputStream().flush();
+                MessageReader reader = new LoggingMessageReader(
+                        new BinaryMessageReader(
+                                new DataInputStream(socket.getInputStream())));
+                MessageWriter writer = new LoggingMessageWriter(
+                        new BinaryMessageWriter(
+                                new DataOutputStream(socket.getOutputStream())));
+                Connection connection = new Connection(reader, writer);
+                connection.run();
+                socket.close();
+            } catch (Exception error) {
+                logger.error(error);
                 try {
-                    final Session session = SessionFactory.instantiate(socket.getInputStream());
-                    if (!(session.getMajorVersion() == 1 && session.getMinorVersion() == 0))
-                        throw new Exception("Version not supported");
-                    logger.info("Accepted " + session);
-                    socket.getOutputStream().write(0); // Accepted
-                    socket.getOutputStream().flush();
-                    MessageReader reader = new LoggingMessageReader(
-                            new BinaryMessageReader(
-                                    new DataInputStream(socket.getInputStream())));
-                    MessageWriter writer = new LoggingMessageWriter(
-                            new BinaryMessageWriter(
-                                    new DataOutputStream(socket.getOutputStream())));
-                    Connection connection = new Connection(reader, writer);
-                    connection.run();
                     socket.close();
-                } catch (Exception error) {
-                    logger.error(error);
-                    try {
-                        socket.close();
-                    } catch (IOException closeError) {
-                        logger.error(closeError);
-                    }
-                }
-            }
-        } catch (Exception error) {
-            logger.error("Closing server", error);
-            if (serverSocket != null) {
-                try {
-                    serverSocket.close();
                 } catch (IOException closeError) {
                     logger.error(closeError);
                 }
             }
         }
     }
-
-    ;
 }
